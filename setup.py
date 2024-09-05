fa8abb31481dad54a20548590309578ce433a854
KAMALSAI369 <tillarikamalsai@gmail.com>
2024-09-05 10:37:26 +0530

    Update requirements.txt

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+from typing import List
+HYPEN_E_DOT='-e .'
+def get_requirements(file_path:str) -> List[str]:
+  requirements=[]
+  with open (file_path) as file_obj:
+      requirements=file_obj.readlines()
+      requirements=[req.replace("\n","") for req in requirements]
+      if HYPEN_E_DOT in requirements:
+          requirements.remove(HYPEN_E_DOT)
+  return requirements
+setup (
+    name = "LSTM Text Generation",
+    version="0.0.1",
+    author="Sai",
+    author_email="tillarikamalsai@gmail.com",
+    install_requires=get_requirements("requirements.txt"),
+    packages=find_packages()
+)
